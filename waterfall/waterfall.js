ec9bd9f426399ad69b1648e9ce73de54e1f6e4ed
faithZZZ <1208674494@qq.com>
2017-09-16 15:16:15 +0800

    新增: 瀑布流图片

--- waterfall/waterfall.js
+++ waterfall/waterfall.js
@@ -27,7 +27,9 @@
 		setItemWidth: function(){
 			var $ele = this.$element,
 				_item = $ele.find('.item'),
-				_w = $ele.width(),
+				_spacingWidth = this.Options.spacingWidth,
+				_spacingHeight = this.Options.spacingHeight,
+				_w = $ele.width() - _spacingWidth * (this.Options.length+1),
 				_tiemW = _w/this.Options.length,
 				_str='';
 			_item.width(_tiemW);
@@ -35,13 +37,15 @@
 		},
 		setItemPosition: function(item){
 			var _arrHeight = this.getArrHeight(item),
-				_w = $(item).eq(0).width() + 20,
+				_w = $(item).eq(0).width(),
+				_spacingWidth = this.Options.spacingWidth,
+				_spacingHeight = this.Options.spacingHeight,
 				_len = this.Options.length;
 				for(var i = 0,len = item.length;i < len;i++){
 					var _minHeight = Math.min.apply(null,_arrHeight),//获取每列排下来最小值
 						_index = _arrHeight.indexOf(_minHeight),
 						_item = item.eq(i),
-						_left = i < _len ? i * _w : _index * _w,
+						_left = i < _len ? i * _w + (i+1) * _spacingWidth : _index * _w,
 						_top = i < _len ? 0 : _minHeight;
 						if(i > _len){
 							_arrHeight[_index] = _minHeight + _item.height();
