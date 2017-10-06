5ee32e99674c8d3d0ddcafe0677a2cb8f7cfed07
faithZZZ <1208674494@qq.com>
2017-10-06 20:27:41 +0800

    处理误删除

--- jquerySourcecode/js/jquery.js
+++ jquerySourcecode/js/jquery.js
@@ -195,10 +195,8 @@ jQuery.extend = jQuery.fn.extend = function() {
 
 jQuery.extend( {
 
-	// Unique for each copy of jQuery on the page
 	expando: "jQuery" + ( version + Math.random() ).replace( /\D/g, "" ),
 
-	// Assume jQuery is ready without the ready module
 	isReady: true,
 
 	error: function( msg ) {
