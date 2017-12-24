db4c845c77e085b9870b823923f03368f00e400c
faithzzz <1208674494@qq.com>
2017-12-24 21:56:57 +0800

    修改: webpack-dev-server hot

--- 工程化/webpack/webpack.config.js
+++ 工程化/webpack/webpack.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 //const WebpackDevServer = require("webpack-dev-server");
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
 	entry: './public/js/app.js',//JavaScript入口的执行文件
@@ -15,7 +16,8 @@ module.exports = {
         contentBase: './views', //本地服务器所加载的页面所在的目录
         historyApiFallback: true, //不跳转
         inline: true,
-        hot: true
+        hot: true,
+        port:9000
     },
 	module: {
 		rules: [
@@ -39,6 +41,9 @@ module.exports = {
 			// 从 .js 文件中提取出来的 .css 文件的名称
       		filename: `[name]_[contenthash:8].css`
     	}),
+    	new CleanWebpackPlugin(['assets']),
+	    //new HtmlWebpackPlugin(),
+    	//new webpack.HotModuleReplacementPlugin(),
     	// new HtmlWebpackPlugin({
      //        template: __dirname + "/app/index.tmpl.html" //new 一个这个插件的实例，并传入相关的参数
      //    }),
