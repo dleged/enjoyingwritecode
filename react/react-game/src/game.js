8de4fc736325d5d00099807a89e4ea3e4e19074a
faithzzz <1208674494@qq.com>
2018-01-12 14:15:17 +0800

    添加：react 小游戏

---
+++ react/react-game/src/game.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import './index.css';
+
+
+
+
+class Game extends Component{
+	render(){
+		return (
+			<div>
+
+			</div>
+		)
+	}
+}
+
+export default Game;
