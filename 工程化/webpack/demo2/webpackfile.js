937fe91b3c0889264ea058771066bb3f6f845fd9
faithzzz <1208674494@qq.com>
2018-01-24 23:38:05 +0800

    demo2

--- 工程化/webpack/demo2/webpackfile.js
+++ 工程化/webpack/demo2/webpackfile.js
@@ -18,13 +18,33 @@ module.exports = {
 			title: 'html-webpack-plugin',
 			template: 'index.html',
 			filename: 'pages/index.html',
-			inject: 'false',
-			date: new Date(),
+			inject: false
 			//压缩
 			// minify: {
 			// 	removeComments: true,
 			// 	collapseWhitespace: true
 			// }
+		}),
+		new HtmlWebpackPlugin({
+			title: 'this is page a',
+			template: 'index.html',
+			filename: 'pages/a.html',
+			inject: 'body',
+			chunks: ['main','a']
+		}),
+		new HtmlWebpackPlugin({
+			title: 'this is page b',
+			template: 'index.html',
+			filename: 'pages/b.html',
+			inject: 'body',
+			chunks: ['b']//引入块
+		}),
+		new HtmlWebpackPlugin({
+			title: 'this is page c',
+			template: 'index.html',
+			filename: 'pages/c.html',
+			inject: 'body',
+			excludeChunks: ['c']//不包括块
 		})
 	]
 }
