d1ed88a486da013d9d28740080b4378ac75bc1c1
faithzzz <1208674494@qq.com>
2018-02-04 23:00:48 +0800

    react

--- react/music/src/index.js
+++ react/music/src/index.js
@@ -5,6 +5,24 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import $ from 'jquery';
 
+$.ajax({
+	url: 'http://searchtip.kugou.com/getSearchTip',
+	data: {
+		MusicTipCount:5,
+		MVTipCount:2,
+		albumcount:2,
+		keyword: '及',
+		callback: 'jQuery191004123277164288508_1517755339994',
+		_: '1517755339999'
+	},
+	dataType: 'jsonp',
+	type: 'POST',
+	success: function(data){
+		console.log(data);
+	}
+})
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
