90fb53bb771e285509fae20033d80930e8c5995c
faithzzz <1208674494@qq.com>
2018-02-07 20:52:52 +0800

    修改server.js

--- react/music/server.js
+++ react/music/server.js
@@ -1,12 +1,24 @@
 const express = require('express');
 const path = require('path');
-const router = require('router');
 const app = express();
+const port = 9000;
 
 app.use(express.static(path.join(__dirname, 'build')));
+
 // /*可以访问所有的路由
 app.get('/*', function (req, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
+// DEBUG:
+app.listen(9000,function(err){
+    if(!err){
+        console.log(
+            `
+            Local:            http://localhost:${port}/
+            On Your Network:  http://192.168.11.100:${port}/
 
-app.listen(9000);
+            `
+        );
+    }
+
+});
