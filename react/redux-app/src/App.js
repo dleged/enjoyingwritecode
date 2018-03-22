176d7b045ea32db32b6e8b36b96080f4a149c688
faithzzz <1208674494@qq.com>
2018-03-22 23:47:09 +0800

    新增bindActionCreators

--- react/redux-app/src/App.js
+++ react/redux-app/src/App.js
@@ -6,6 +6,7 @@ import Goods from './components/Goods';
 import { connect } from 'react-redux';
 import { additive,subtraction } from './action/caculatorAction';
 import { updateUser } from './action/userAction';
+import { bindActionCreators } from 'redux'
 
 class App extends Component {
   constructor(props){
@@ -16,7 +17,6 @@ class App extends Component {
     this.props.onUpdateUser(e.target.value);
   }
   render() {
-    this.props.dispatch({type:'user:UPDATEUSER',payload:{user: 'sunmmer'}})
     return (
         <div className="App">
           <header className="App-header">
@@ -26,7 +26,7 @@ class App extends Component {
               <p className="App-intro">
                 To get started, edit <code>src/App.js</code> and save to reload.
               </p>
-              <input ref={(e)=>{this.dom = e.target}} onChange={this.updateUser} defaultValue={this.props.user}/>
+              <input onChange={this.updateUser} defaultValue={this.props.user}/>
               <p>{this.props.user}</p>
               <Caculator value={this.props.caculator.value} additive={this.props.onAdditive} subtraction={this.props.onSubtraction}/>
         </div>
@@ -34,16 +34,21 @@ class App extends Component {
   }
 }
 
-
+// 注入 dispatch 和全局 state
+// 不要这样做！这会导致每次 action 都触发整个 TodoApp 重新渲染，你做的所有性能优化都将付之东流。
+// 最好在多个组件上使用 connect()，每个组件只监听它所关联的部分 state
 const mapStateToProps = state => ({
   user: state.user,
   products: state.products,
   caculator: state.caculator
 })
-const mapDispatchToProps = state => ({
-  onUpdateUser: updateUser,
-  onAdditive: additive,
-  onSubtraction: subtraction
+
+//注入 dispatch 和 userAction
+//绑定dispatch的action，返回函数新函数，其实就是执行dispatch(action(...arg))
+const mapDispatchToProps = dispatch => ({
+  onUpdateUser: bindActionCreators(updateUser,dispatch),
+  onAdditive: bindActionCreators(additive),
+  onSubtraction: bindActionCreators(subtraction)
 })
 
 export default connect(mapStateToProps,mapDispatchToProps)(App);
--- react/redux-app/src/action/userAction.js
+++ react/redux-app/src/action/userAction.js
@@ -3,8 +3,6 @@ export const UPDATE_USER = "user:UPDATEUSER";
 export function updateUser (newUser){
 	return{
 		type: UPDATE_USER,
-		payload: {
-			user: newUser
-		}
+		payload: newUser
 	}
 }
