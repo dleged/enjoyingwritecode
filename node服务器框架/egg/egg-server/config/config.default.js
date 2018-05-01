edbd1aa016bdcd02cc665fda0271c39e5e48a14a
faithzzz <1208674494@qq.com>
2018-05-01 17:37:26 +0800

    设置默认配置文件

--- node服务器框架/egg/egg-server/config/config.default.js
+++ node服务器框架/egg/egg-server/config/config.default.js
@@ -1,12 +1,30 @@
 'use strict';
 
+const path = require('path');
+
 module.exports = appInfo => {
   const config = exports = {};
 
   // use for cookie sign key, should change to your own and keep security
   config.keys = appInfo.name + '_1524926843611_2835';
 
-  // add your config here
+  config.static = {
+    prefix: '/public/',
+    dir: path.join(appInfo.baseDir, 'app/public'),
+    dynamic: true,
+    preload: false,
+    maxAge: 31536000, //in prod env, 0 in other envs
+    buffer: true //in prod env, false in other envs
+
+  }
+
+  // 添加 view 配置
+  config.view = {
+    defaultViewEngine: 'nunjucks',
+    mapping: {
+      '.tpl': 'nunjucks',
+    },
+  };
   config.middleware = [];
 
   return config;
