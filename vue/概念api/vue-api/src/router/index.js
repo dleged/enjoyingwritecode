108a953c44fa3a22cf55383f52d376c33bca5899
faithzzz <1208674494@qq.com>
2018-05-19 23:26:26 +0800

    vue v-on

--- vue/概念api/vue-api/src/router/index.js
+++ vue/概念api/vue-api/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import Von from '@/components/v-on'
 
 Vue.use(Router)
 
@@ -12,9 +13,9 @@ export default new Router({
       component: HelloWorld
     },
     {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      path: '/v-on',
+      name: 'v-on指令',
+      component: Von
     }
   ]
 })
