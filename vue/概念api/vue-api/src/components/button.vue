c7265c0a534cb166b17c0b18628ab9157f9c6e86
faithzzz <1208674494@qq.com>
2018-05-22 22:38:02 +0800

    vue 组件

---
+++ vue/概念api/vue-api/src/components/button.vue
@@ -0,0 +1,22 @@
+<template lang="html">
+	<button type="button" @click="count += 1" name="button" >you clicked me {{count}} times</button>
+</template>
+
+<script>
+export default {
+	name: 'button',
+	data () {
+		return {
+			count : 0
+		}
+	}
+}
+</script>
+
+<style lang="css">
+	button{
+		color: white;
+		background: red;
+		padding: 10px;
+	}
+</style>
---
+++ vue/概念api/vue-api/src/page/index.vue
@@ -0,0 +1,16 @@
+<template lang="html">
+		<Button></Button>
+		<Button></Button>
+		<Button></Button>
+		<Button></Button>
+</template>
+
+<script>
+	import Button from '../components/button';
+	export default {
+		name: 'index'
+	}
+</script>
+
+<style lang="css">
+</style>
