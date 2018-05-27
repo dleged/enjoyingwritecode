bdfbf4bec92d54c343d73e39a507f6622084b592
faithzzz <1208674494@qq.com>
2018-05-27 23:58:35 +0800

    vue

--- vue/概念api/vue-api/src/components/list.vue
+++ vue/概念api/vue-api/src/components/list.vue
@@ -3,7 +3,7 @@
 		<h1>{{ title }}</h1>
 		<ul>
 			<!-- 当它们处于同一节点，v-for 的优先级比 v-if 更高，这意味着 v-if 将分别重复运行于每个 v-for 循环中。当你想为仅有的一些项渲染节点时，这种优先级的机制会十分有用 -->
-				<template v-for = "(item,i) in map" v-if="item.officeRecords > 50000">
+				<template v-for = "(item,i) in maps" v-if="item.officeRecords > 50000">
 					<li :key="item.name">{{ item.name }}</li>
 					 <li :key="item.officeRecords" @click="item.officeRecords += 1"> 票房：{{ item.officeRecords }}</li>
 				</template>
@@ -26,7 +26,18 @@ export default {
 		}
 	},
 	beforecreated() {
+		// push会更新视图
 		this.map.push({name: '盗墓笔记',officeRecords: 5000000});
+	},
+	computed(){
+		mpas: function() {
+			return this.map.map((item,index) => {
+				return {
+					name: item.name,
+					officeRecords: item.officeRecords + '票'
+				}
+			})
+		}
 	}
 }
 </script>
