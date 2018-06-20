b5c91d7184a934449b9425c60671ac16fa21be18
faithzzz <1208674494@qq.com>
2018-06-20 10:02:57 +0800

    看得到我的log吗

--- node/event/event.js
+++ node/event/event.js
@@ -8,3 +8,7 @@ emitter.on('sum',function(num1 = 1,num2 = 2){
 emitter.emit('sum');
 emitter.emit('sum', 2, 3);
 emitter.emit('sum', 100, 300);
+
+emitter.emit('sum');
+emitter.emit('sum', 2, 3);
+emitter.emit('sum', 100, 300);
