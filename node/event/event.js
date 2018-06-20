07868e94b98fe1ed062d45d0eb84db3af7a3ac96
faithzzz <1208674494@qq.com>
2018-06-20 10:02:24 +0800

    -am命令

---
+++ node/event/event.js
@@ -0,0 +1,10 @@
+const events = require('events');
+const emitter = new events.EventEmitter();
+
+emitter.on('sum',function(num1 = 1,num2 = 2){
+    console.log(num1 + num2);
+})
+
+emitter.emit('sum');
+emitter.emit('sum', 2, 3);
+emitter.emit('sum', 100, 300);
