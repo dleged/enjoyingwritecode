5cf90aabdd60afa1e8a14c1db56bac74c9fc0ffa
faithzzz <1208674494@qq.com>
2018-06-21 21:54:11 +0800

    http

--- node/http/server.js
+++ node/http/server.js
@@ -1,12 +1,15 @@
 const http = require('http');
 // const cp = require('child_process');
+const util = require('util');
+const url = require('url');
 const opn = require('opn');
 const PORT = 9999;
 
-http.createServer((request, resopnse) => {
-    resopnse.writeHead(200, { 'Content-Type': 'text/html' });
-    resopnse.write('<div style="color: red;font-size: 20px;">HELLO NODE.JS</div>');
-    resopnse.end('set by node http server!');
+http.createServer((request, response) => {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
+    response.write('<div style="color: red;font-size: 20px;">HELLO NODE.JS</div>');
+    response.write(util.inspect(url.parse(request.url,true)));
+    response.end('set by node http server!');
 }).listen(PORT);
 
 
