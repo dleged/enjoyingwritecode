d9da9da239ed5a596576306c9917818caebaa177
yimu <1208674494@qq.com>
2018-08-23 20:00:00 -0600

    mobx

---
+++ react相关/mobx/app.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import PropTypes from 'prop-types'
+
+class App extends React.Component {
+	render () {
+		<div>app</div>
+	}
+}
+
+ReactDOM.render(<App />,document.querySelector('#root'));
