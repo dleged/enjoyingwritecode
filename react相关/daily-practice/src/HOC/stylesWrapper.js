aada088514bb7f808366126b404b68203744bc18
yimu <1208674494@qq.com>
2018-12-05 21:50:56 +0800

    Merge branch 'master' of https://github.com/dleged/front-end-demo

--- react相关/daily-practice/src/HOC/stylesWrapper.js
+++ react相关/daily-practice/src/HOC/stylesWrapper.js
@@ -4,16 +4,19 @@ import buttonStyles from '../styles/buttonStyles';
 
 
 const translateStyles = (props) => {
-	let _styles = {...buttonStyles.default};
+	let _styles = buttonStyles.default;
 	if(props.disable){
 		_styles = {..._styles,...buttonStyles.disable}
 	}
+
+	//合并style
+	_styles = {..._styles,...props.style}
 	return _styles;
 }
 
 const stylesWrapper = (wrapperComponent) => {
 		return function(props){
-			return <Button style={translateStyles(props)}></Button>
+			return <Button {...props} style={translateStyles(props)}></Button>
 		}
 }
 
--- react相关/daily-practice/src/components/Button.js
+++ react相关/daily-practice/src/components/Button.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 class Button extends React.Component {
 	render () {
-		return <Button {...this.props}>{this.props.child || '按钮'}</Button>
+		return <button {...this.props}>{this.props.child || '按钮'}</button>
 	}
 }
 
