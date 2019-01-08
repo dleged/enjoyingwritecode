2e63ffd0c2105d6afab965dd66c44dd090db2fa2
yimu <1208674494@qq.com>
2019-01-08 22:41:08 +0800

    瀑布流事件

---
+++ plugins/waterfall/waterfall.js
@@ -0,0 +1,7 @@
+(funcrion(t){
+	function waterfall(ele){
+		let imgFallBox = document.querySelector('ele')[0];
+		let imgItem = imgFallBox.querySelector(' > img');
+	}
+	t.waterfall = waterfall;
+})(window)
