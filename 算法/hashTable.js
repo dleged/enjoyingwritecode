9354ca51ed55220309aae60f98a2bd3bf237e3c2
yimu <1208674494@qq.com>
2019-01-20 21:51:10 +0800

    算法

---
+++ 算法/hashTable.js
@@ -0,0 +1,39 @@
+class HashTable{
+	constructor(...args){
+		let limitLen = args[0] || 10;
+		this.hashTable = new Array(limitLen)
+	}
+
+	_getHash(value){
+		var t = 0;
+		for(var i = 0,len = value.length;i<len;i++){
+			t = t + value.charCodeAt(i);
+		}
+		return t % len;
+	}
+
+	add(value){
+		return this.hashTable[this._getHash(value)] = value;
+	}
+
+	get(value){
+		return this.hashTable[this._getHash(value)];
+	}
+
+	remove(){
+		return delete this.hashTable[this._getHash(value)];
+	}
+
+	print(){
+		return this.hashTable;
+	}
+}
+
+let hash = new HashTable;
+
+hash.add('html');
+hash.add('css');
+hash.add('javascript');
+
+console.log(hash.get('html'));
+console.log(hash.print());
