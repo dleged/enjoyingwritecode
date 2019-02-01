2d37d9252b992b4d53c6f1820d300b607667a9b5
范端端 <duanduanf@gmail.com>
2019-02-01 15:43:11 +0800

    parcel

---
+++ react相关/react-componets/lib/modal/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+
+class Modal extends React.Component {
+	render () {
+
+	}
+}
+
+Modal.defaultProps = {
+	
+}
+
+
+
+export default Modal;
--- 工程化/parcel/dist/script.eb6db66e.js
+++
@@ -1,8 +0,0 @@
-parcelRequire=function(e,r,n){var t="function"==typeof parcelRequire&&parcelRequire,i="function"==typeof require&&require;function u(n,o){if(!r[n]){if(!e[n]){var f="function"==typeof parcelRequire&&parcelRequire;if(!o&&f)return f(n,!0);if(t)return t(n,!0);if(i&&"string"==typeof n)return i(n);var c=new Error("Cannot find module '"+n+"'");throw c.code="MODULE_NOT_FOUND",c}a.resolve=function(r){return e[n][1][r]||r};var l=r[n]=new u.Module(n);e[n][0].call(l.exports,a,l,l.exports)}return r[n].exports;function a(e){return u(a.resolve(e))}}u.isParcelRequire=!0,u.Module=function(e){this.id=e,this.bundle=u,this.exports={}},u.modules=e,u.cache=r,u.parent=t;for(var o=0;o<n.length;o++)u(n[o]);return u}({2:[function(require,module,exports) {
-
-},{}],4:[function(require,module,exports) {
-"use strict";Object.defineProperty(exports,"__esModule",{value:!0});var e=require("../style/main.css"),t=u(e);function u(e){return e&&e.__esModule?e:{default:e}}exports.default=function(){console.log(t.default.main)};
-},{"../style/main.css":2}],3:[function(require,module,exports) {
-"use strict";var e=require("./main"),u=r(e);function r(e){return e&&e.__esModule?e:{default:e}}(0,u.default)();
-},{"./main":4}]},{},[3])
-//# sourceMappingURL=/script.3330b2e1.map
