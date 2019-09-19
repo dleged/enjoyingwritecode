345d7512561f47ad81fe479112788cb55af37a17
yimu <yimu@WangShifengdeMacBook-Pro.local>
2019-09-19 23:41:33 +0800

    19

--- 每日一练/2019-09/19.js
+++ 每日一练/2019-09/19.js
@@ -1,12 +1,14 @@
 //查找字符串中出现最多的字符和个数
 
-var str = 'adsadasdasdsadasdsadsdfdsfdsfsd';
+var str = 'adsadasdasdsadasdsadsdfdsfdsfsdz';
 
-str = str.split('').sort((a,b) => a < b).join('');
+str = str.split('').sort().join('');
 
 var num = 0;
 var chart = '';
+console.log(str);
 str.replace(/(\w)\1+/g,($0,$1) => {
+  console.log($1);
   if($0.length > num){
     num = $0.length;
     chart = $1;
