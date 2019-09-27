9c84643b056bc184958e1e5fd3239875f645b477
yimu <yimu@WangShifengdeMacBook-Pro.local>
2019-09-27 09:36:21 +0800

    26

---
+++ 每日一练/2019-09/26.js
@@ -0,0 +1,19 @@
+//实现apply
+function exmple(name,age){
+  return `my name is ${name},${age} old!`;
+}
+
+Function.prototype._apply = function(contex){
+  contex = contex || window;
+  contex.fn = this;
+  let arg = arguments[1] || [];
+  // for(let i = 1,len = arguments.length; i < len; i++){
+  //   arg.push(arguments[i])
+  // }
+
+  let result = contex.fn(...arg);
+  delete contex.fn;
+  return typeof result ? result : this;
+}
+
+exmple._apply(null,['yimu',18]);
