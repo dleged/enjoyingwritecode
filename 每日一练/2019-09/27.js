5676b61c28f7ce559c7233bc614ca8484c52c836
yimu <yimu@WangShifengdeMacBook-Pro.local>
2019-09-28 22:42:49 +0800

    28

--- 每日一练/2019-09/27.js
+++ 每日一练/2019-09/27.js
@@ -1,1 +1,21 @@
 //踢球累了，睡觉
+Function.prototype._bind = function(context){
+  let _this = [].shift.argumets.call(argumets);
+  let args = argumets;
+  if(typeof context !== 'function') return throw console.error('argument must be a function!');
+
+  let fn = function(arguments){
+    context.apply(typeof this === fn ? tihs : _this,args.concat([].shift.call(arguments));
+  }
+
+  return fn
+}
+
+var name = 'yimu';
+
+function exmple(){
+  console.log(this.name);
+}
+
+exmple.bind(window)(); //yimu
+exmple._bind(window)();
