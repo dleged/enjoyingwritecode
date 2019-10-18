607ce43ecd73ef5e55044000379206f319dae907
yimu <1208674494@qq.com>
2019-10-18 19:41:31 +0800

    10.18

--- react相关/react-webpack/src/components/Main.js
+++ react相关/react-webpack/src/components/Main.js
@@ -1,5 +1,6 @@
 require('normalize.css/normalize.css');
 require('styles/App.css');
+import { StickyContainer, Sticky } from 'react-sticky';
 
 import React from 'react';
 
@@ -9,6 +10,27 @@ class AppComponent extends React.Component {
   render() {
     return (
       <div className="index">
+        <StickyContainer>
+          {/* Other elements can be in between `StickyContainer` and `Sticky`,
+          but certain styles can break the positioning logic used. */}
+          <Sticky>
+            {({
+              style,
+
+              // the following are also available but unused in this example
+              isSticky,
+              wasSticky,
+              distanceFromTop,
+              distanceFromBottom,
+              calculatedHeight
+            }) => (
+              <header style={style}>
+                Generator Header
+              </header>
+            )}
+          </Sticky>
+          {/* ... */}
+        </StickyContainer>
         <img src={yeomanImage} alt="Yeoman Generator" />
         <div className="notice">Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
