77090bcb995ad7f6b70d1ee1a8b732b6faaa0f7b
yimu <1208674494@qq.com>
2019-10-21 22:12:33 +0800

    10.21

---
+++ CHANGELOG.md
@@ -0,0 +1,20 @@
+# Version NaN (2019-10-21)
+
+## Features
+* Initial commit for changelog generator ([1a43d9](https://github.com/jackyef/changelog-generator/commit/1a43d98bfc043d9da00aaa3dab8d5d9e63351fd7))
+
+# Version NaN (2019-10-21)
+
+## Features
+* Initial commit for changelog generator ([1a43d9](https://github.com/jackyef/changelog-generator/commit/1a43d98bfc043d9da00aaa3dab8d5d9e63351fd7))
+
+# Version NaN (2019-10-21)
+
+## Features
+* Initial commit for changelog generator ([1a43d9](https://github.com/jackyef/changelog-generator/commit/1a43d98bfc043d9da00aaa3dab8d5d9e63351fd7))
+
+# Version NaN (2019-10-21)
+
+## Features
+* Initial commit for changelog generator ([1a43d9](https://github.com/jackyef/changelog-generator/commit/1a43d98bfc043d9da00aaa3dab8d5d9e63351fd7))
+
--- index.js
+++ index.js
@@ -1,15 +1,67 @@
-'use strict'
-const fs = require('fs');
-const path = require('path');
+const child = require("child_process");
+const fs = require("fs");
 
-console.log(1);
+const output = child
+  .execSync(`git log --format=%B%H----DELIMITER----`)
+  .toString("utf-8");
 
-function stattag () {
-  let stat = fs.statSync(path.join(__dirname,'list.md'));
-  var mtime = stat.mtime.getTime().toString(16)
-  var size = stat.size.toString(16)
-  return '"' + size + '-' + mtime + '"'
-}
+const commitsArray = output
+  .split("----DELIMITER----\n")
+  .map(commit => {
+    const [message, sha] = commit.split("\n");
 
-console.log(stattag());
-console.log(2);
+    return { sha, message };
+  })
+  .filter(commit => Boolean(commit.sha));
+
+// const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
+// const currentVersion = Number(require("./package.json").version);
+// const newVersion = currentVersion + 1;
+// let newChangelog = `# Version ${newVersion} (${
+//   new Date().toISOString().split("T")[0]
+// })\n\n`;
+//
+// const features = [];
+// const chores = [];
+//
+// commitsArray.forEach(commit => {
+//   if (commit.message.startsWith("feature: ")) {
+//     features.push(
+//       `* ${commit.message.replace("feature: ", "")} ([${commit.sha.substring(
+//         0,
+//         6
+//       )}](https://github.com/jackyef/changelog-generator/commit/${
+//         commit.sha
+//       }))\n`
+//     );
+//   }
+//   if (commit.message.startsWith("chore: ")) {
+//     chores.push(
+//       `* ${commit.message.replace("chore: ", "")} ([${commit.sha.substring(
+//         0,
+//         6
+//       )}](https://github.com/jackyef/changelog-generator/commit/${
+//         commit.sha
+//       }))\n`
+//     );
+//   }
+// });
+//
+// if (features.length) {
+//   newChangelog += `## Features\n`;
+//   features.forEach(feature => {
+//     newChangelog += feature;
+//   });
+//   newChangelog += '\n';
+// }
+//
+// if (chores.length) {
+//   newChangelog += `## Chores\n`;
+//   chores.forEach(chore => {
+//     newChangelog += chore;
+//   });
+//   newChangelog += '\n';
+// }
+//
+// // prepend the newChangelog to the current one
+// fs.writeFileSync("./CHANGELOG.md", `${newChangelog}${currentChangelog}`);
