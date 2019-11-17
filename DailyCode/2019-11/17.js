5e808e1c21c55239fd8d8938e8449da85269f974
yimu <duanduanf@gmail.com>
2019-11-17 22:58:46 +0800

    10.17

---
+++ DailyCode/2019-11/17.js
@@ -0,0 +1,34 @@
+//归并排序
+// 复杂度 O(nlogn)
+function mergeSort(arr){
+  let len = arr.length;
+  if(len < 2){
+    return arr;
+  }
+  let mid = Math.floor(len/2);
+  let left = arr.slice(0,mid);
+  let right = arr.slice(mid);
+  return merge(mergeSort(left),mergeSort(right));
+  function merge(left,right){
+    let result = [];
+    while(left.length && right.length){
+      if(left[0] <= right[0]){
+        result.push(left.shift());
+      }else{
+        result.push(right.shift());
+      }
+    }
+
+    while(left.length){
+      result.push(left.shift());
+    }
+
+    while(right.length){
+      result.push(right.shift());
+    }
+
+    return result;
+  }
+}
+
+console.log(mergeSort([12,3,346,45,32,23,12,43,65,8,11,9,67,56,45,34]));
