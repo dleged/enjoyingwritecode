9271fbe141e98223110f7245e0994635f725fbc2
yimu <1208674494@qq.com>
2020-04-04 22:34:24 +0800

    feat: install mongodb

--- Hybird/meal/App.tsx
+++ Hybird/meal/App.tsx
@@ -2,22 +2,23 @@ import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+// import TodoList from './src/pages/todolist';
 
-function HomeScreen() {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-    </View>
-  );
-}
 
 const Stack = createStackNavigator();
+
+function HomesCreen(){
+  return <View>
+    <Text>hello react native app!</Text>
+  </View>
+}
+
 
 function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Home" component={HomesCreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
--- workbox/workbox-config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  "globDirectory": "build/",
-  "globPatterns": [
-    "**/*.{js,css}"
-  ],
-  "swDest": "build/sw.js"
-};
