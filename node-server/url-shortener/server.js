cd83c4c592fc277257b59da25cc45dd15cc46295
yimu <1208674494@qq.com>
2020-04-08 20:31:53 +0800

    feat: mongodb install success

--- node-server/url-shortener/server.js
+++ node-server/url-shortener/server.js
@@ -1,34 +1,48 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const ShortUrl = require('./models/shortUrl')
-const app = express()
+const cookieParser = require('cookie-parser')
+let token = require('./token');
+let tokenid = 0;
+const app = express();
 
 mongoose.connect('mongodb://localhost:27017/urlShortener', {
   useNewUrlParser: true, useUnifiedTopology: true
 })
 
 app.set('view engine', 'ejs')
+app.use(cookieParser())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', async (req, res) => {
-  const shortUrls = await ShortUrl.find()
-  res.render('index', { shortUrls: shortUrls })
+  const shortUrls = await ShortUrl.find();
+  let access_token = `url_shortener${Date.now()}`;
+  token[access_token] = tokenid++;
+
+  res.cookie('access_token',access_token);
+  res.render('index', { shortUrls: shortUrls });
+  
 })
 
 app.post('/shortUrls', async (req, res) => {
-  await ShortUrl.create({ full: req.body.fullUrl })
-
+  console.log(req.body)
+  await ShortUrl.create({ full: req.body.fullurl })
   res.redirect('/')
 })
 
 app.get('/:shortUrl', async (req, res) => {
   const shortUrl = await ShortUrl.findOne({ short: req.params.shortUrl })
-  if (shortUrl == null) return res.sendStatus(404)
+  console.log(req.query);
 
-  shortUrl.clicks++
-  shortUrl.save()
+  if (shortUrl == null) return res.sendStatus(404);
+  shortUrl.clicks++;
+  shortUrl.save();
 
-  res.redirect(shortUrl.full)
+  console.dir(req.cookies.access_token);
+  console.log(token[req.cookies.access_token]);//模拟token
+
+  // res.render('index', { shortUrls: shortUrls });
+  res.redirect(shortUrl.full);
 })
 
 app.listen(process.env.PORT || 5000);
