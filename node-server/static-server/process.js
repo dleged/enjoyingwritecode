4b42f26ad8109752b0dabf96d5de23249021ebac
yimu <1208674494@qq.com>
2020-04-29 23:04:12 +0800

    feat: 子进程启动服务

---
+++ node-server/static-server/process.js
@@ -0,0 +1,17 @@
+// const { exec } = require('child_process');
+
+// exec('node index.js', (err, stdout, stderr) => {
+//   if (err) {
+//     console.error(`exec error: ${err}`);
+//     return;
+//   }
+
+//   console.log(`Number of files ${stdout}`);
+// });
+
+const {exec} = require('child_process');
+
+let project = exec('node',['index.js']);
+console.log(project.pid);
+
+
