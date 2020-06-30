8ee51b97b818cb9034d1643ca4d7d208063ce0a8
yimu <15658866967@163.com>
2020-06-30 19:46:14 +0800

    daily coding

--- node/cluster/index.js
+++ node/cluster/index.js
@@ -1,39 +1,25 @@
-const cluster = require('cluster');
-const http = require('http');
+const cluster = require("cluster");
+const express = require("express");
+
+
+const app = express();
+const numCPUs = require("os").cpus().length;
 
 if (cluster.isMaster) {
-
-  // 跟踪 http 请求。
-  let numReqs = 0;
-  setInterval(() => {
-    console.log(`请求的数量 = ${numReqs}`);
-  }, 1000);
-
-  // 对请求计数。
-  function messageHandler(msg) {
-    if (msg.cmd && msg.cmd === 'notifyRequest') {
-      numReqs += 1;
-    }
-  }
-
-  // 启动 worker 并监听包含 notifyRequest 的消息。
-  const numCPUs = require('os').cpus().length;
   for (let i = 0; i < numCPUs; i++) {
     cluster.fork();
   }
 
-  for (const id in cluster.workers) {
-    cluster.workers[id].on('message', messageHandler);
-  }
-
+  cluster.on("exit", (worker, code, signal) => {
+    conosle.log(`process ${workder.process.pid} is exit!`);
+  });
 } else {
+  app.listen(8000, () => {
+    console.log(`process ${process.pid} is stared!`);
+  });
 
-  // 工作进程有一个 http 服务器。
-  http.Server((req, res) => {
-    res.writeHead(200);
-    res.end('你好世界\n');
-
-    // 通知主进程接收到了请求。
-    process.send({ cmd: 'notifyRequest' });
-  }).listen(8000);
+  app.use("/", (req, res, next) => {
+    console.log(`visible ${process.pid}!`,2);
+    res.end(`process ${process.pid} is stared!`);
+  });
 }
