894f305cabdcc00082584cb49963fd6a1807fd7a
yimu <15658866967@163.com>
2020-08-04 09:34:04 +0800

    #skip

--- node/watch/server.js
+++ node/watch/server.js
@@ -1,8 +1,10 @@
 const http = require('http');
 
 http.createServer(function(req, res){
-  res.writeHead(200, { 'Content-Type': 'text/html'})
-  res.end('hello world111');
+  res.writeHead(200, { 'Content-Type': 'text/html'});
+  res.write('hello');
+  res.write(' ');// write 可以多次调用
+  res.end('world');// 必须调用 end，否则客户端一直拿不到结果
 }).listen(9999,() => {
   console.log('server listening at port 9999...')
 });
