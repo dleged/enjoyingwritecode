3816a2e752cb3996e6475555a6f563de97eaae91
yimu <>
2021-03-10 23:14:50 +0800

    feat: daily code

--- DailyCode/2020-11/09-斐波那契数列.js
+++ DailyCode/2020-11/09-斐波那契数列.js
@@ -1,4 +1,24 @@
-// 
-function fibonacci(index){
-  
+// 1 1 2 3 5 8 13
+function fibonacci(index) {
+  if (index === 1 || index === 2) {
+    return 1;
+  }
+
+  return fibonacci(index - 2) + fibonacci(index - 1);
 }
+
+// OR
+
+var queue = [1,1];
+function fibonacci2(index) {
+  if(index === 1 || index === 2) {
+    return 1;
+  }
+  for(let i = 2; i <= index; i++) {
+    queue[i] = queue[i - 2] + queue[i - 1];
+  }
+  
+  return queue[index-1];
+}
+
+console.log(fibonacci2(4));
