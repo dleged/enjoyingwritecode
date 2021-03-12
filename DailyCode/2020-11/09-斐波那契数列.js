d49222ca4f9b4ed93ded8e2eb5bc7a46c7d03e85
yimu <>
2021-03-12 16:18:14 +0800

    feat: daily code

--- DailyCode/2020-11/09-斐波那契数列.js
+++ DailyCode/2020-11/09-斐波那契数列.js
@@ -14,7 +14,7 @@ function fibonacci2(index) {
   if(index === 1 || index === 2) {
     return 1;
   }
-  for(let i = 2; i <= index; i++) {
+  for(let i = 2; i < index; i++) {
     queue[i] = queue[i - 2] + queue[i - 1];
   }
   
--- node/process/stdin.js
+++ node/process/stdin.js
@@ -1,7 +1,6 @@
 const chalk = require('/usr/local/lib/node_modules/chalk');
 process.stdin.setEncoding('utf8');
 
-
 process.stdout.write('是否进行升级(y/n)?');
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
