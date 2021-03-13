eb68716b0fbcd2e7f6139bf98a937bc19ef514bb
yimu <>
2021-03-13 20:53:00 +0800

    feat: daily code

--- DailyCode/2020-11/04-异步串行.js
+++ DailyCode/2020-11/04-异步串行.js
@@ -15,3 +15,33 @@
 */
 // 需要按照 a,b,延迟1秒,c,延迟1秒,d,e, done 的顺序打印
 
+function createFlow(effects) {
+  const queue = [...effects.flat()];
+
+  return {
+    run: async function (callback) {
+      for (let fn of queue) {
+        if (fn.isFlow) {
+          await fn.run();
+        } else {
+          await fn();
+        }
+      }
+      if (callback) {
+        callback();
+      }
+    },
+    isFlow: true,
+  };
+}
+
+const delay = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
+const subFlow = createFlow([() => delay(1000).then(() => console.log("c"))]);
+createFlow([
+  () => console.log("a"),
+  () => console.log("b"),
+  subFlow,
+  [() => delay(1000).then(() => console.log("d")), () => console.log("e")],
+]).run(() => {
+  console.log("done");
+});
