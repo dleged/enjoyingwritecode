52ec2312b94d60aa9034969c9fab7a6fec174edf
dleged <15658866967@163.com>
2024-02-27 23:01:04 +0800

    docs: remove files

--- handwritten-question/数组深度.js
+++ handwritten-question/数组深度.js
@@ -4,12 +4,7 @@ let arr = [1,3,3,[4,6,7,[5,6,7,43,[23,4]]]]
 
 let max = 0;
 function getDeep(arr, level) {
-  max = Math.max(level,max);
-  for (let i = 0; i < arr.length; i++) {
-    if(Array.isArray(arr)){
-      getDeep(arr[i],level+1);
-    }
-  }
+
 }
 
 getDeep(arr, 0);
