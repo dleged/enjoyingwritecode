38c0fcb26af11b3b7dadf3798f831b18d86f64c6
dleged <15658866967@163.com>
2024-03-20 12:14:20 +0800

    docs: coding

--- algorithm/19. 删除链表的倒数第 N 个结点.js
+++ algorithm/19. 删除链表的倒数第 N 个结点.js
@@ -1,8 +1,29 @@
 // 给定一个链表，删除链表的倒数第 n 个节点，并返回链表的头结点。
 
+function ListNode(val, next) {
+  this.val = val;
+  this.next = next;
+}
 
 function removeNthFromEnd(head, n) {
 
+  let dummy = new ListNode(0, head);
+  let slow = dummy;
+  let fast = dummy;
+
+  while (n--) {
+    fast = fast.next;
+  }
+
+  while (fast.next) {
+    fast = fast.next;
+    slow = slow.next;
+  }
+
+  slow.next = slow?.next?.next || null;
+
+
+  return dummy.next;
 }
 
 
@@ -14,17 +35,17 @@ head1.next = new ListNode(2);
 head1.next.next = new ListNode(3);
 head1.next.next.next = new ListNode(4);
 head1.next.next.next.next = new ListNode(5);
-console.log(printList(removeNthFromEnd(head1, 2))); // 应输出: 1->2->3->5
+console.log((removeNthFromEnd(head1, 2))); // 应输出: 1->2->3->5
 
 // 测试用例2
 // 输入：head = 1->2, n = 2
 // 输出：2
 let head2 = new ListNode(1);
 head2.next = new ListNode(2);
-console.log(printList(removeNthFromEnd(head2, 2))); // 应输出: 2
+console.log((removeNthFromEnd(head2, 2))); // 应输出: 2
 
 // 测试用例3
 // 输入：head = 1, n = 1
 // 输出：[]
 let head3 = new ListNode(1);
-console.log(printList(removeNthFromEnd(head3, 1))); // 应输出: []
+console.log((removeNthFromEnd(head3, 1))); // 应输出: []
--- code/base/deepClone.js
+++ code/base/deepClone.js
@@ -1,66 +1,151 @@
-function deepClone(target, map = new Map()) {
 
-  let dist = target;
+// function deepClone(target){
+//   const weakMap = new WeakMap();
+//   function clone(target){
 
-  if (typeof target === 'object') {
+//     if(target == undefined){
+//       return target;
+//     }
 
-    // 处理循环引用
-    if (map.get(target)) {
-      return map.get(target);
-    }
+//     let dist = {};
+//     if(typeof target !== 'object'){
+//       return target;
+//     }else{
 
-    if (target instanceof Array) {
-      dist = [];
-    } else if (target instanceof Date) {
-      dist = new Date(target);
-    }
-    else if (target instanceof RegExp) {
-      dist = new RegExp(target.souce, target.flags);
-    } else if (target instanceof Function) {
-      dist = function () {
-        return target(...arguments);
-      }
-    } else if (target instanceof Map) {
-      return new Map(target);
-    } else {
-      dist = {}
-    }
+//       if(weakMap.has(target)){
+//         return weakMap.get(target);
+//       }
 
-    map.set(target, dist);
-    // 处理自有属性
-    for (let key in target) {
-      if (target.hasOwnProperty(key)) {
-        dist[key] = deepClone(target[key], map);
-      }
-    }
+//       if(target instanceof Array){
+//         dist = [];
+//       }else if(target instanceof Function){
+//         dist = function(){target.apply(this,arguments)}
+//       }else if(target instanceof Date){
+//         dist = new Date(target);
+//       }else if(target instanceof RegExp){
+//         const {source,flags,lastIndex} = target;
+//         dist = new RegExp(source,flags);
+//         dist.lastIndex = lastIndex;
+//       }else if(target instanceof Map){
+//         dist = new Map(target);
+//       }else if(target instanceof Symbol){
+//         dist = Object(Symbol.prototype.valueOf.call(target));
+//       }else if(target instanceof Set){
+//         dist = new Set(target);
+//       }
+//       weakMap.set(target,dist);
+//       for(let key in target){
+//         if(target.hasOwnProperty(key)){
+//           dist[key] = clone(target[key]);
+//         }
+//       }
+//       return dist;
+//     }
+//   }
+//   return clone(target);
+// }
 
-    return dist;
+function initObjectWithType(source) {
+  if (source instanceof Array) {
+    return new Array();
   } else {
-    return target;
+    return Object.create(Object.getPrototypeOf(source));
   }
-
 }
 
-module.exports = deepClone;
+function deepClone(source) {
 
-const map = new Map();
-map.set('a', 1);
-map.set('b', 2);
+  if (source === null || typeof source !== 'object') return source;
+  if (source === undefined) return source;
 
+  let map = new Map();
+  let target = initObjectWithType(source);
+  let queue = [{ target: target, source }];
 
-const obj = [
-  {
-    fuc: (arg1, arg2) => console.log(arg1, arg2),
-    date: new Date(),
-    RegExp: /\d+/g
-  },
-  1,
-  [1, 2, 3],
-  map,
-]
+  function clone(source) {
+    if (source === null || typeof source !== 'object') return source;
+    if (source === undefined) return source;
 
-obj.push(obj);
+    let target;
 
-const other = deepClone(obj);
-obj[0] = 0;
-console.log(other, obj);
+    if (source instanceof Array) {
+      target = new Array();
+    } else if (source instanceof Map) {
+      target = new Map(source);
+    } else if (source instanceof Set) {
+      target = new Set(source);
+    } else if (source instanceof Date) {
+      target = new Date(source);
+    } else if (source instanceof Function) {
+      target = source.bind({});
+    } else if (source instanceof RegExp) {
+      target = new RegExp(source.source, source.flags);
+      target.lastIndex = source.lastIndex;
+    } else if (source instanceof Symbol) {
+      target = Object(Symbol.prototype.valueOf.call(source));
+    } else {
+      // 创建一个具有同样原型的对象
+      target = Object.create(Object.getPrototypeOf(source));
+    }
+
+    return target;
+  };
+
+  while (queue.length) {
+    let { target, source } = queue.shift();
+
+    // 遍历可枚举
+    for (let key in source) {
+      // 排除原型属性
+      if (!source.hasOwnProperty(key)) return;
+
+      if (map.has(source[key])) {
+        target[key] = map.get(source[key]);
+      } else {
+        if (typeof source[key] === 'object') {
+          // 目标对象的值赋值为原对象的值
+          target[key] = clone(source[key]);
+          // 缓存拷贝过的值
+          map.set(source[key], target[key]);
+          // 对象其他的属性加入队列中
+          queue.push({ target: target[key], source: source[key] });
+
+        } else {
+          target[key] = source[key];
+        }
+      }
+    }
+
+  }
+
+  return target;
+}
+
+let target = {
+  time: new Date(),
+  object: { new: '1' },
+  array: [1, 2, 3],
+  function: function (a) { console.log(a) },
+  Symbol: Symbol('instance'),
+  regx: /[a-z]/mg,
+  map: new Map().set('name', 'a'),
+}
+
+target.circula = target;
+
+targetCopy = deepClone(target)
+targetCopy.object = { 'old': '1' };
+targetCopy.array = [3, 2, 1];
+console.log(target, 22, targetCopy);
+
+let targetArr = [{
+  time: new Date(),
+  object: { new: '1' },
+  array: [1, 2, 3],
+  function: function (a) { console.log(a) },
+  Symbol: Symbol('instance'),
+  regx: /[a-z]/mg,
+  map: new Map().set('name', 'a'),
+}];
+
+console.log(deepClone(targetArr), targetArr)
